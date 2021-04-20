ccb2ca23cb516daa4899bd0290051e64882317d9
Jake Melville <jakemelville@ymail.com>
2021-04-20 11:45:39 -0400

    cleaned up link color in the aside component

--- portfolio/src/components/Nav/index.js
+++ portfolio/src/components/Nav/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Resume from './assets/images/JakeMelville_resume.pdf'
+import './assets/style/style.css'
 
 function Nav() {
     return (
-        <ul className="col">
-            <li className="mb-3">About Me</li>
-            <li className="mb-3"><a href="/apps">My Applications</a></li>
-            <li className="mb-3"><a href={Resume} target='_blank'>Resume</a></li>
-            <li className='mb-2'>Contact Me</li>
+        <ul id="sideBar" className="col">
+            <li id="abtMe" className="mb-3">About Me</li>
+            <li id="myApps" className="mb-3"><a href="/apps">My Applications</a></li>
+            <li id="myResume" className="mb-3"><a href={Resume} target='_blank'>Resume</a></li>
+            <li id="meContact" className='mb-2'>Contact Me</li>
         </ul>
     )
 }
